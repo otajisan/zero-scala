cc4d8d38a8dc99ece86a756f2e7460cf69779f56
otajisan <taji.taji.otajisan@gmail.com>
2017-08-25 15:44:47 +0900

    initial commit

---
+++ src/main/scala/com/zeroscala/BootConfig.scala
@@ -0,0 +1,8 @@
+package com.zeroscala
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+
+@SpringBootApplication
+@EnableAutoConfiguration
+class BootConfig
